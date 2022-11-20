2656489d8be1b41f6ef1ad5551572d7f85728d3c
Prakhar Gupta <gprakhar@iisc.ac.in>
2022-11-20 19:31:15 +0530

    some thing thing

--- globvar.h
+++ globvar.h
@@ -1,9 +1,12 @@
 #ifndef __GLOBVAR_H__
 #define __GLOBVAR_H__
 
+#define MAX_VCPUS                        (64) // because uint64_t
+
 #define	 PORT_SERIAL			 (0x3f8)
 #define	 PORT_WAIT_USER_CODE_MAPPING	 (0x3f9)
 #define	 PORT_HLT			 (0x3fa)
 #define	 PORT_PRINT_REGS		 (0x3fb)
-#define  PORT_MY_ID                      (0x3fc)
+#define  PORT_MY_ID                      (0x3fc) // 0x3fd
+#define  PORT_MSG                        (0x3fe) // 0x3ff
 #endif
